86f5bd3ea12ec913c158f95afbcfeee0e8c6a36c
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2025-06-19 22:56:11 +0530

    Add files via upload

---
+++ mini/AtmOperations.java
@@ -0,0 +1,56 @@
+ package mini;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class AtmOperations implements AtmInterface {
+	
+	Atm atm = new Atm();
+	Map<Double,String> ministmt=new HashMap<>();
+	
+	@Override
+	public void viewBalance() {	
+		System.out.println("Available Balance is: " + atm.getBalance());
+	}
+
+	@Override
+	public void withdrawAmount(double withdrawAmount) {
+		if(withdrawAmount % 500 == 0)
+		{
+			if(withdrawAmount <= atm.getBalance())
+			{
+				ministmt.put(withdrawAmount,"Amount withdrawn");
+				System.out.println("collect the cash" +withdrawAmount);
+				atm.setBalance(atm.getBalance()-withdrawAmount);
+				viewBalance();
+			}
+			else
+			{
+				System.out.println("Insufficient Balance!");
+			}
+		}
+		else
+		{
+			System.out.println("please enter the amount in multipple of 500");
+		}
+		
+	}
+
+	@Override
+	public void depositAmount(double depositAmount) {
+		ministmt.put(depositAmount," Amount Deposited");
+		System.out.println(depositAmount + " Deposited Succesfully!");
+		atm.setBalance(atm.getBalance() + depositAmount);
+		viewBalance();
+	}
+
+	@Override
+	public void viewMiniStatement() {
+		for (Double key : ministmt.keySet()) {
+		    String value = ministmt.get(key);
+		    System.out.println(key + " - " + value);
+		}
+	}
+
+}
